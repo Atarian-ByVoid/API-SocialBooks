6b7006283d972a29f1eab0d7a96a55c36fbc0c25
Atarian-ByVoid <cgomespimentel7@gmail.com>
2022-09-29 15:01:59 -0300

    new feature 1.0

--- src/main/java/br/fepi/socialbooks/resoucers/LivrosResources.java
+++ src/main/java/br/fepi/socialbooks/resoucers/LivrosResources.java
@@ -15,14 +15,19 @@ public class LivrosResources {
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
 	public List<Livro> listar() {
 		
-		Livro livro1 = new Livro("A arte da guerra");
-		Livro livro2 = new Livro("1984");
-		Livro livro3 = new Livro("O pequeno príncipe");
-		Livro livro4 = new Livro("Dona flor e seus 2 maridos");
-		Livro livro5 = new Livro("Dona flor e seus 2 maridos");
+		Livro livro1 = new Livro("Manual do Imério");
+		Livro livro2 = new Livro("DUNA");
+		Livro livro3 = new Livro("Game Of Thrones");
+		Livro livro4 = new Livro("Uma odisséia no espaço");
+		Livro livro5 = new Livro("Artemis");
+		Livro livro6 = new Livro("Diário perdido");
+		Livro livro7 = new Livro("zelda Ocarina Of Time");
+		Livro livro8 = new Livro("A queda de gondolin");
+		Livro livro9 = new Livro("Dracula");
+
 
 		
-		Livro[] livros = {livro1, livro2, livro3, livro4,livro5};
+		Livro[] livros = {livro1, livro2, livro3, livro4,livro5,livro6,livro7,livro8,livro9};
 		
 		return Arrays.asList(livros);
 				
